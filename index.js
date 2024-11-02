e21b8d6437303d9487e67cc32012ac3560ffd85a
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-11-02 19:09:52 +0100

    Exercise 3.7

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const dotenv = require('dotenv/config')
 const { personsRoutes } = require('./src/routes/index.js')
 
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
 
 //Routes:
 app.use(personsRoutes)
