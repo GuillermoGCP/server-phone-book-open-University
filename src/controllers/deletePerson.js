3f4ebc4b7ea2505ad0f22340ac22589d71c35793
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-11-08 18:07:22 +0100

    Exercise 3.15

--- src/controllers/deletePerson.js
+++ src/controllers/deletePerson.js
@@ -1,19 +1,22 @@
-const storedPersons = require('../db/persons.json')
-const fs = require('fs/promises')
+const Person = require('../db/models/PersonsModel.js')
 
 const deletePerson = async (req, res) => {
     const { id } = req.params
 
-    //Check if person exists:
-    const person = storedPersons.find((person) => person.id === id)
-    if (!person) return res.status(404).send(`<p>Person not found</p>`)
-
-    const remainingPeople = storedPersons.filter((person) => person.id !== id)
-    // Save new data:
-    await fs.writeFile(
-        './src/db/persons.json',
-        JSON.stringify(remainingPeople, null, 2)
-    )
-    res.status(204).end()
+    Person.findByIdAndDelete(id)
+        .then((person) => {
+            if (person) {
+                res.status(200).json({
+                    id: person._id.toString(),
+                    name: person.name,
+                })
+            } else {
+                res.status(404).send('Person not found')
+            }
+        })
+        .catch((error) => {
+            console.error('Error al eliminar la persona:', error)
+            res.status(500).send('Error deleting person')
+        })
 }
 module.exports = deletePerson
